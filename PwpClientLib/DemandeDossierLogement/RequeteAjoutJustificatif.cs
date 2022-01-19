f5853774aeff235a917738bcc6cd814aed9fa695
Emmanuel <emmanueldurin@gmail.com>
2022-01-19 11:27:29 +0100

    Ajout RequeteAjoutJustificatif

---
+++ PwpClientLib/DemandeDossierLogement/RequeteAjoutJustificatif.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PwpClientLib.DemandeDossierLogement
+{
+  public class RequeteAjoutJustificatif
+  {
+    [Required]
+    public string IdTransaction { get; set; }
+    /// <summary>
+    /// Numéro unique du dossier sur 18 caractères
+    /// </summary>
+    [Required]
+    [StringLength(18)]
+    public string NumUnique { get; set; }
+    public Guichet Guichet { get; set; }
+    public Utilisateur Utilisateur { get; set; }
+    public List<Fichier> Fichiers { get; set; }
+  }
+}
